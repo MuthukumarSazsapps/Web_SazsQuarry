7501cbd054325ad31427cc77ddec8e6f639a2ccf
MuthukumarSazsapps <kumar@sazsapps.com>
2024-05-15 20:48:27 +0530

    buil in gitignore

--- src/components/App/Modal.jsx
+++ src/components/App/Modal.jsx
@@ -5,16 +5,16 @@ const Modal = () => {
   return (
     <div>
 
-      <div class="modal fade" id="myModal">
-        <div class="modal-dialog  modal-dialog-centered">
-        <div class="modal-content" style={{backgroundColor:"transparent ",borderColor:"transparent"}}>
-          <div class="">
-          {/* <button type="button" class="close  btn-light" data-dismiss="modal" aria-label="Close">
+      <div className="modal fade" id="myModal">
+        <div className="modal-dialog  modal-dialog-centered">
+        <div className="modal-content" style={{backgroundColor:"transparent ",borderColor:"transparent"}}>
+          <div className="">
+          {/* <button type="button" className="close  btn-light" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button> */}
-            <button type="button" class="btn-close  btn-light " data-bs-dismiss="modal" style={{backgroundColor:'white',color:"blue",pointerEvents: "auto"}}></button>
+            <button type="button" className="btn-close  btn-light " data-bs-dismiss="modal" style={{backgroundColor:'white',color:"blue",pointerEvents: "auto"}}></button>
           </div>
-          <div class="modal-body">
+          <div className="modal-body">
           <ContactForm rtl={""} />
           </div>
 
